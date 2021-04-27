fe9315486a38c2a7645cec470da1f9ae577ae027
Mitch Hornsby <hornsbym19@mail.wlu.edu>
2021-04-26 18:43:16 -0700

    Removed some stock node files.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          amandawahlers.com is currently under construction.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Come back soon!
+        </p>
       </header>
     </div>
   );
